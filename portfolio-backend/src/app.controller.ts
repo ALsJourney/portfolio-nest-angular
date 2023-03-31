e3e586a15e90f7d4d545fb152ba3ab81cb4a5e6f
ALsJourney <aldin.dino@outlook.com>
2023-04-01 00:06:10 +0200

    First commit

---
+++ portfolio-backend/src/app.controller.ts
@@ -0,0 +1,13 @@
+import {Controller, Get, Res} from '@nestjs/common';
+import { AppService } from './app.service';
+import { join } from 'path'
+import { Response } from "express";
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('*')
+  serveApp(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', 'portfolio', 'dist', 'portfolio', 'index.html'))
+  }
+}
---
+++ portfolio-backend/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {join} from "path";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'portfolio', 'dist', 'portfolio', 'index.html'))
+  await app.listen(3000);
+
+}
+bootstrap();
